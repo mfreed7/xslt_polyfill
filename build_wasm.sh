9a1b6890cbb9516f2c32cd707a0266f1d4567b45
Mason Freed <masonf@chromium.org>
2025-07-01 16:19:36 -0700

    Working build script

--- build_wasm.sh
+++ build_wasm.sh
@@ -10,27 +10,44 @@ XML2_INSTALL_DIR="${BUILD_DIR}/libxml2-install"
 XSLT_INSTALL_DIR="${BUILD_DIR}/libxslt-install"
 OUT_FILE="${BUILD_DIR}/xslt_wasm.js"
 
-# --- Compile C glue code to WASM ---
-emcc -O3 \
+# --- Set PKG_CONFIG_PATH for emscripten ---
+# This tells pkg-config where to find the .pc files for our compiled libs
+export PKG_CONFIG_PATH="${XML2_INSTALL_DIR}/lib/pkgconfig:${XSLT_INSTALL_DIR}/lib/pkgconfig"
+
+# --- Verify that pkg-config can find the libraries ---
+if ! pkg-config --exists libxml-2.0 || ! pkg-config --exists libxslt; then
+    echo "Error: pkg-config could not find libxml-2.0 or libxslt." >&2
+    echo "This means the libraries were not built and installed correctly into the ./build directory." >&2
+    echo "PKG_CONFIG_PATH is: ${PKG_CONFIG_PATH}" >&2
+    exit 1
+fi
+
+# Get compiler and linker flags directly from pkg-config
+# This is the robust way to find headers and libraries
+CFLAGS=$(pkg-config --cflags libxml-2.0 libxslt)
+LIBS=$(pkg-config --libs libxml-2.0 libxslt)
+
+# echo "Found CFLAGS: ${CFLAGS}"
+# echo "Found LIBS: ${LIBS}"
+
+emcc -O1 -g4 \
   src/transform.c \
   -o ${OUT_FILE} \
+  ${CFLAGS} \
   -s WASM=1 \
   -s MODULARIZE=1 \
   -s SINGLE_FILE=1 \
   -s ALLOW_MEMORY_GROWTH=1 \
-  # -s SAFE_HEAP=1 \
-  # -s ASSERTIONS=1 \
-  # -s INITIAL_MEMORY=134217728 \
-  # -s STACK_SIZE=5242880 \
+  -s SAFE_HEAP=1 \
+  -s ASSERTIONS=1 \
   -s EXPORT_NAME="'createXSLTTransformModule'" \
   -s EXPORTED_FUNCTIONS="['_transform', '_malloc', '_free']" \
   -s EXPORTED_RUNTIME_METHODS="['cwrap', 'UTF8ToString']" \
-  -I"${XSLT_INSTALL_DIR}/include" \
-  -I"${XML2_INSTALL_DIR}/include/libxml2" \
-  -L"${XSLT_INSTALL_DIR}/lib" \
-  -L"${XML2_INSTALL_DIR}/lib" \
-  -lxslt \
-  -lexslt \
-  -lxml2
+  ${LIBS}
+
+#   # -s SAFE_HEAP=1 \
+#   # -s ASSERTIONS=1 \
+#   # -s INITIAL_MEMORY=134217728 \
+#   # -s STACK_SIZE=5242880 \
 
 echo "--- ${OUT_FILE} (embedded WASM) ---"
